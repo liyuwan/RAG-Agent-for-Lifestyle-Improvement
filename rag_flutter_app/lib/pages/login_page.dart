288486e577be8396452c85e113186dde4fe06ab5
Min Swan Pyae <122963201+LuRyan301@users.noreply.github.com>
2025-03-11 13:32:56 +0700

    google_login updated

--- rag_flutter_app/lib/pages/login_page.dart
+++ rag_flutter_app/lib/pages/login_page.dart
@@ -79,7 +79,7 @@ class _LoginPageState extends State<LoginPage> {
         width: double.infinity,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFF00C9A7), Color(0xFF008080)],
+            colors: [Color(0xFF66E0CA), Color(0xFF008080)], //Color(0xFF00C9A7)
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
@@ -87,125 +87,128 @@ class _LoginPageState extends State<LoginPage> {
         child: Center(
           child: SingleChildScrollView(
             padding: const EdgeInsets.all(25.0),
-            child: Card(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-              elevation: 10,
-              child: Padding(
-                padding: const EdgeInsets.all(25),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.account_circle,  // You can change this to any icon you like
-                      size: 100,  // Adjust the size as needed
-                      color: Color(0xFF008080),  // You can change the color
-                    ),
-                    const SizedBox(height: 20),  // Add some spacing
-                    Text(
-                      _isRegistering ? 'Create an Account' : 'Welcome Back!',
-                      style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.black87),
-                    ),
-                    const SizedBox(height: 20),
-                    _buildTextField(_emailController, 'Email', Icons.email, false),
-                    const SizedBox(height: 15),
-                    _buildTextField(_passwordController, 'Password', Icons.lock, true, isConfirm: false),
-                    if (_isRegistering) const SizedBox(height: 15),
-                    if (_isRegistering) _buildTextField(_confirmPasswordController, 'Confirm Password', Icons.lock, true, isConfirm: true),
-                    const SizedBox(height: 30),
-                    _isLoading
-                        ? const CircularProgressIndicator(color: Color(0xFF008080))
-                        : Column(
-                            children: [
-                              ElevatedButton(
-                                onPressed: _isRegistering ? _register : _login,
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xFF008080),
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),  // Same as Google Sign In
-                                  minimumSize: const Size(double.infinity, 50), // Make the button's width take the full space and fixed height
-                                  elevation: 3, // Optional: to match the "Sign in with Google" button elevation
-                                ),
-                                child: Text(
-                                  _isRegistering ? 'Register' : 'Login',
-                                  style: const TextStyle(fontSize: 18, color: Colors.white),
-                                ),
-                              ),
-
-                              const SizedBox(height: 15),
-
-                              // "Or Log in with" section
-                              Row(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/login_logo.png',  // Ensure the logo is placed in the `assets` directory
+                  height: 200,
+                ),
+                const SizedBox(height: 20),  // Add some spacing
+                Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  elevation: 10,
+                  child: Padding(
+                    padding: const EdgeInsets.all(25),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          _isRegistering ? 'Create an Account' : 'Welcome Back!',
+                          style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.black87),
+                        ),
+                        const SizedBox(height: 20),
+                        _buildTextField(_emailController, 'Email', Icons.email, false),
+                        const SizedBox(height: 15),
+                        _buildTextField(_passwordController, 'Password', Icons.lock, true, isConfirm: false),
+                        if (_isRegistering) const SizedBox(height: 15),
+                        if (_isRegistering) _buildTextField(_confirmPasswordController, 'Confirm Password', Icons.lock, true, isConfirm: true),
+                        const SizedBox(height: 30),
+                        _isLoading
+                            ? const CircularProgressIndicator(color: Color(0xFF008080))
+                            : Column(
                                 children: [
-                                  Expanded(
-                                    child: Divider(
-                                      color: Colors.black54, 
-                                      thickness: 1,
-                                      indent: 20,
-                                      endIndent: 10,
+                                  ElevatedButton(
+                                    onPressed: _isRegistering ? _register : _login,
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: const Color(0xFF008080),
+                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),  // Same as Google Sign In
+                                      minimumSize: const Size(double.infinity, 50), // Make the button's width take the full space and fixed height
+                                      elevation: 3, // Optional: to match the "Sign in with Google" button elevation
+                                    ),
+                                    child: Text(
+                                      _isRegistering ? 'Register' : 'Login',
+                                      style: const TextStyle(fontSize: 18, color: Colors.white),
                                     ),
                                   ),
-                                  const Text(
-                                    'or log in with',
-                                    style: TextStyle(color: Colors.black54, fontSize: 16),
+
+                                  const SizedBox(height: 15),
+
+                                  // "Or Log in with" section
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: Divider(
+                                          color: Colors.black54, 
+                                          thickness: 1,
+                                          indent: 20,
+                                          endIndent: 10,
+                                        ),
+                                      ),
+                                      const Text(
+                                        'or log in with',
+                                        style: TextStyle(color: Colors.black54, fontSize: 16),
+                                      ),
+                                      Expanded(
+                                        child: Divider(
+                                          color: Colors.black54, 
+                                          thickness: 1,
+                                          indent: 10,
+                                          endIndent: 20,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  Expanded(
-                                    child: Divider(
-                                      color: Colors.black54, 
-                                      thickness: 1,
-                                      indent: 10,
-                                      endIndent: 20,
+                                  const SizedBox(height: 15),
+
+                                  ElevatedButton.icon(
+                                    onPressed: () async {
+                                      UserCredential? userCredential = await AuthService().signInWithGoogle();
+                                      if (userCredential != null) {
+                                        Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => const MainPage()),
+                                        );
+                                      } else {
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                          const SnackBar(content: Text('Google Sign-In failed')),
+                                        );
+                                      }
+                                    },
+                                    icon: Image.asset('assets/google_logo.png', height: 24),
+                                    label: const Text('Sign in with Google', style: TextStyle(color: Colors.black87)),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.white,
+                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),  // Same padding as Login/Register
+                                      minimumSize: const Size(double.infinity, 50), // Fixed width and height
+                                      elevation: 3,
+                                    ),
+                                  ),
+
+                                  const SizedBox(height: 10),
+                                  TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        _isRegistering = !_isRegistering;
+                                        _emailController.clear();
+                                        _passwordController.clear();
+                                        _confirmPasswordController.clear();
+                                      });
+                                    },
+                                    child: Text(
+                                      _isRegistering ? 'Already have an account? Login' : 'Don’t have an account? Register',
+                                      style: const TextStyle(fontSize: 16, color: Colors.black54),
                                     ),
                                   ),
                                 ],
                               ),
-                              const SizedBox(height: 15),
-
-
-                              ElevatedButton.icon(
-                                onPressed: () async {
-                                  UserCredential? userCredential = await AuthService().signInWithGoogle();
-                                  if (userCredential != null) {
-                                    Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(builder: (context) => const MainPage()),
-                                    );
-                                  } else {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(content: Text('Google Sign-In failed')),
-                                    );
-                                  }
-                                },
-                                icon: Image.asset('assets/google_logo.png', height: 24),
-                                label: const Text('Sign in with Google', style: TextStyle(color: Colors.black87)),
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white,
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),  // Same padding as Login/Register
-                                  minimumSize: const Size(double.infinity, 50), // Fixed width and height
-                                  elevation: 3,
-                                ),
-                              ),
-
-                              const SizedBox(height: 10),
-                              TextButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _isRegistering = !_isRegistering;
-                                    _emailController.clear();
-                                    _passwordController.clear();
-                                    _confirmPasswordController.clear();
-                                  });
-                                },
-                                child: Text(
-                                  _isRegistering ? 'Already have an account? Login' : 'Don’t have an account? Register',
-                                  style: const TextStyle(fontSize: 16, color: Colors.black54),
-                                ),
-                              ),
-                            ],
-                          ),
-                  ],
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         ),
@@ -233,11 +236,25 @@ class _LoginPageState extends State<LoginPage> {
                 },
               )
             : null,
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(25.0)),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
         filled: true,
-        fillColor: Colors.grey[100],
+        fillColor: Colors.transparent,
       ),
       obscureText: isPassword ? (isConfirm ? _obscureConfirmPassword : _obscurePassword) : false,
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
